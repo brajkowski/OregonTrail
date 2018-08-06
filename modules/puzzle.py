ddfbe5768c5234bbed2c95c317aaa63e3b00bc2c
brajkowski <brandon.rajkowski@gmail.com>
2018-08-06 16:23:31 -0600

    fixed guess tracking in puzzle

--- modules/puzzle.py
+++ modules/puzzle.py
@@ -20,21 +20,22 @@ def did_win():
   # Initialize.
   number = random.randint(1,10)
   guesses = 3
+  guesses_remaining = guesses
   options = [1,2,3,4,5,6,7,8,9,10]
   
-  print("Guess a number between 1 and 10 ({} guesses remaining)".format(guesses))
+  print("Guess a number between 1 and 10 ({} guesses remaining)".format(guesses_remaining))
   for i in range(guesses):
     response = io.get_input_int_protected(options)
-    guesses -= 1
+    guesses_remaining -= 1
     
     # Handle winning and give hints.
     if response == number:
       print("You win!")
       return True
-    elif response > number:
-      print("Try a lower number ({} guesses remaining)".format(guesses))
-    elif response < number:
-      print("Try a higher number ({} guesses remaining)".format(guesses))
+    elif response > number and i < guesses - 1:
+      print("Try a lower number ({} guesses remaining)".format(guesses_remaining))
+    elif response < number and i < guesses - 1:
+      print("Try a higher number ({} guesses remaining)".format(guesses_remaining))
   
   # Handle losing.
   print("Better luck next time.")
